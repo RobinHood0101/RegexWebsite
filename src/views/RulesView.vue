73f13259378fb4c4a3299b8498fb37fcb1d8dc02
RobinHood0101 <robinkammer@proton.me>
2025-03-31 19:42:29 +0200

    init

---
+++ src/views/RulesView.vue
@@ -0,0 +1,23 @@
+<script>
+    export default {
+        data() {
+            return {
+                rules: [
+                    { title: "test", description: "123", example: "sadfsdaf" },
+                    { title: "test123", description: "dfs123", example: "sasadfsdfdfsdaf" },
+                    { title: "46456test", description: "123324", example: "sadfsdafsdfsd" },
+                ]
+            }
+        }
+    }
+</script>
+
+<template>
+    <main>
+        <template v-for="rule in rules">
+            <h2>{{ rule.title }}</h2>
+            <p>{{ rule.description }}</p>
+            <p>{{ rule.example }}</p>
+        </template>
+    </main>
+</template>
